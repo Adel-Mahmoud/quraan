1f79025453e4f510c861310092972bebd81a1c79
Alaa Adel <107191894+Adel-Mahmoud@users.noreply.github.com>
2024-01-23 07:41:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,56 +1,57 @@
-const cacheName = "cache1"; // Change value to force update
+const cacheName = 'my-cache';
 
-self.addEventListener("install", event => {
-	// Kick out the old service worker
-	self.skipWaiting();
-
-	event.waitUntil(
-		caches.open(cacheName).then(cache => {
-			return cache.addAll([
-				"/",
-                '192.png',
-                '512.png',
-                'index.html',
-                'assets/css/custome-style.css',
-                'assets/css/templatemo-572-designer.css',
-                'vendor/bootstrap/css/bootstrap.min.css',
-                'assets/css/fontawesome.css',
-                'logo.png',
-                'assets/images/bg1.jpg',
-                '1.jpg',
-                'vendor/jquery/jquery.min.js',
-                'vendor/bootstrap/js/bootstrap.min.js',
-                'assets/js/custom.js',
-                'assets/js/script.js',
-			]);
-		})
-	);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll([
+        	"/",
+          '192.png',
+          '512.png',
+          'index.html',
+          'assets/css/custome-style.css',
+          'assets/css/templatemo-572-designer.css',
+          'vendor/bootstrap/css/bootstrap.min.css',
+          'assets/css/fontawesome.css',
+          'logo.png',
+          'assets/images/bg1.jpg',
+          '1.jpg',
+          'vendor/jquery/jquery.min.js',
+          'vendor/bootstrap/js/bootstrap.min.js',
+          'assets/js/custom.js',
+          'assets/js/script.js',
+      ]);
+    })
+  );
 });
 
-self.addEventListener("activate", event => {
-	// Delete any non-current cache
-	event.waitUntil(
-		caches.keys().then(keys => {
-			Promise.all(
-				keys.map(key => {
-					if (![cacheName].includes(key)) {
-						return caches.delete(key);
-					}
-				})
-			)
-		})
-	);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== cacheName) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-	event.respondWith(
-		caches.open(cacheName).then(cache => {
-			return cache.match(event.request).then(response => {
-				return response || fetch(event.request).then(networkResponse => {
-					cache.put(event.request, networkResponse.clone());
-					return networkResponse;
-				});
-			})
-		})
-	);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.command === 'trigger-broadcast') {
+    self.clients.matchAll().then((clients) => {
+      clients.forEach((client) => {
+        client.postMessage({ command: 'broadcast-update' });
+      });
+    });
+  }
 });
